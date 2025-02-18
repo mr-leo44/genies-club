9d672bcfa7b7cf4af1d33c098f1b694d27a4fecd
mr-leo44 <lionnelkaniki44@gmail.com>
2025-02-18 06:17:57 +0100

    add navbar component

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    
+  <div class="h-100vh min-h-screen bg-gray-300">
+    <Navbar />
   </div>
+  
 </template>
+<script setup lang="ts">
+import Navbar from "~/components/Navbar.vue"
+</script>
