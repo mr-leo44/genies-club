43ebaa422cff6178ae80ad4462e7cc05f60c5f87
mr-leo44 <lionnelkaniki44@gmail.com>
2025-02-28 01:09:17 +0100

    feat: update Header

--- components/Header.vue
+++ components/Header.vue
@@ -1,22 +1,17 @@
 <template>
   <header class="h-screen bg-slate-50">
-    <Navbar />
-    <div class="flex flex-col max-w-3xl mx-auto w-full h-screen items-center pt-24">
-      <h1 class="font-bold text-5xl text-center">
+    <div class="flex flex-col max-w-6xl mx-auto w-full h-screen items-center pt-4">
+      <h1 class="font-bold text-7xl text-center">
         Transformez vos idées en expériences inoubliables
       </h1>
-      <p class="my-6 text-sm text-center font-medium">
+      <p class="my-6 text-xl text-center font-medium">
         prêt à voir l'extraordinaire prendre vie sous vos yeux ? <br/> Découvrez comment chaque détail devient une étincelle de magie, <br/> chaque projet une histoire à partager.
       </p>
       <div class="my-4">
-        <img src="~/assets/images/hero.png" class="size-80" />
+        <img src="~/assets/images/hero.png" class="size-60" />
       </div>
     </div>
   </header>
   
 </template>
-
-<script setup lang="ts">
-import Navbar from "~/components/Navbar.vue"
-
-</script>
+<script setup lang="ts"></script>
