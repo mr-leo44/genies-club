d91a5f16c2bc15feab3cafff3325669ad35ce5c0
mr-leo44 <lionnelkaniki44@gmail.com>
2025-03-14 12:25:21 +0100

    update About.vue

--- components/About.vue
+++ components/About.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="relative h-screen w-full overflow-hidden card">
-    <div class="absolute inset-0 z-2 h-full w-full bg-black opacity-0 overlay"></div>
-    <div class="flex flex-col space-y-4 left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] z-12 text-white absolute texts">
-      <h4 class="font-bold text-6xl">Un Laboratoire</h4>
-      <p class="max-w-8xl">
+    <div class="absolute inset-0 z-2 h-full w-full bg-slate-500 opacity-0 overlay"></div>
+    <div class="flex flex-col space-y-4 left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] z-12 text-white absolute">
+      <h4 class="font-bold text-6xl text">Un Laboratoire</h4>
+      <p class="max-w-8xl text">
         Créée à Kinshasa le 9 janvier 2010, GC est une association qui est basée sur la création, l'animation, et l'accompagnement d'événements. Elle rassemble en ceci les jeunes artistes (musiciens, marionnettistes, compositeurs, cinéastes, danseurs, comédiens, performeurs), les techniciens, animateurs. GC se veut être un laboratoire pour créer des activités artistiques et culturelles. Il est divisé en quatre sous-groupes.
       </p>
-      <button class="px-3 py-2 bg-cyan-600 text-white font-semibold w-fit rounded-md">Voir nos réalisations</button>
+      <button class="px-3 py-2 bg-cyan-600 text-white font-semibold w-fit rounded-md text">Voir nos réalisations</button>
     </div>
-    <img src="~/assets/images/laboratory.jpg" class="h-auto object-cover" />
+    <img src="~/assets/images/laboratory.jpg" alt="laboratory image" class="h-auto object-cover" />
   </div>
 </template>
 
@@ -18,7 +18,7 @@ import { onMounted } from 'vue'
 const { $gsap } = useNuxtApp()
 
 onMounted(() => {
-  $gsap.set(".texts", {
+  $gsap.set(".text", {
     y: 80,
     opacity: 0
   })
@@ -27,16 +27,23 @@ onMounted(() => {
     scrollTrigger: {
       trigger: ".card",
       start: "top top",
-      end: "+=1500",
+      end: "+=763",
       scrub: true,
       pin: true
     }
   })
-
+  
   tl
   .to(".overlay", { opacity: 0.75 })
   .to(".card", { "clip-path": "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)" }, 0)
-  .to(".texts", { y: 0, opacity: 1 }, 0)
+  .to(".text", { 
+    ease: "power4.out",
+    y: 0,
+    duration: 0.75,
+    opacity: 1,
+    delay: 1, 
+    stagger: 0.5,
+  }, 0)
 })
 
 </script>
