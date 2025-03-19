3b2cf1d137e55119f71b01a3f7da224370d3b5fe
mr-leo44 <lionnelkaniki44@gmail.com>
2025-03-19 07:52:38 +0100

    update gsap client

--- plugins/gsap.ts
+++ plugins/gsap.ts
@@ -1,12 +1,25 @@
-import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
-export default defineNuxtPlugin(() => {
-  gsap.registerPlugin(ScrollTrigger)
+if (typeof window !== 'undefined') {
+  gsap.registerPlugin(ScrollTrigger);
+}
 
-  return {
-    provide: {
-      gsap
-    }
-  }
-})
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(gsap);
+  nuxtApp.provide('gsap', gsap);
+  // nuxtApp.provide('ScrollTrigger', ScrollTrigger);
+});
+
+// import gsap from 'gsap'
+// import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+// export default defineNuxtPlugin(() => {
+//   gsap.registerPlugin(ScrollTrigger)
+
+//   return {
+//     provide: {
+//       gsap
+//     }
+//   }
+// })
